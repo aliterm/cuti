c6da08654e812a7e5132794ef181e722c22b402a
aliterm <me@situsali.com>
2024-12-25 09:39:06 +0700

    update fetcher

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,11 @@
-import { Button } from "flowbite-react";
-
+import { Button } from 'flowbite-react'
 
 export default function Home() {
   return (
     <div className="bg-blue-200">
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Click me</Button>
-
-    </main>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Button>Click me</Button>
+      </main>
     </div>
-  );
+  )
 }
---
+++ frontend/src/helpers/fetcher.ts
@@ -0,0 +1,32 @@
+import { APIDefaultResponse, APIResponse, FetcherParams } from '@/interfaces/fetcher'
+
+const fetcher = async <T = unknown>(params: FetcherParams): Promise<APIResponse<T>> => {
+  try {
+    const { headers, method, endpoint, body, cache } = params
+    const defaultHeaders: HeadersInit = {
+      'Content-Type': 'application/json',
+      ...headers,
+    }
+
+    const apiURL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3030'
+
+    const defaultCache: RequestCache = cache ? cache : 'no-cache'
+    const url = `${apiURL}/${endpoint}`
+    const res = await fetch(url, { method, headers: defaultHeaders, body, cache: defaultCache })
+    const json = await res.json()
+
+    return {
+      statusCode: json?.status_code ?? 500,
+      message: json?.message ?? 'Something went wrong',
+      data: json?.data ?? undefined,
+      pagination: json?.pagination ?? undefined,
+    }
+  } catch (error: unknown) {
+    const err = error as APIDefaultResponse
+    return {
+      statusCode: err?.statusCode || 500,
+      message: err?.message ?? 'Something went wrong',
+    }
+  }
+}
+export default fetcher
