2bdb01e5b365d5ea8fe5a2aed6736b2565064bbf
aliterm <me@situsali.com>
2024-12-25 15:19:43 +0700

    update tambah pegawai

--- frontend/src/app/dashboard/employees/page.tsx
+++ frontend/src/app/dashboard/employees/page.tsx
@@ -2,6 +2,7 @@ import fetcher from '@/helpers/fetcher'
 import { cookies } from 'next/headers'
 import { Employee } from '@/interfaces/employee'
 import { ListEmployees } from './listEmployee'
+import AddEmployee from './addEmployee'
 
 export default async function EmployeesPage() {
   const token = (await cookies()).get('token')?.value
@@ -18,6 +19,7 @@ export default async function EmployeesPage() {
   return (
     <>
       <h1 className="text-2xl font-bold mb-5">Daftar Pegawai</h1>
+      <AddEmployee token={token!} />
       <ListEmployees employees={employees} />
     </>
   )
